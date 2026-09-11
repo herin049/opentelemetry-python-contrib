a7d9afdb48057a00d76e3decc765b34cb00919d0
Lukas Hering <lukasheringb@gmail.com>
2026-09-10 22:35:55 -0500

    fix lint error

--- instrumentation/opentelemetry-instrumentation-valkey/tests/test_valkey.py
+++ instrumentation/opentelemetry-instrumentation-valkey/tests/test_valkey.py
@@ -1,6 +1,8 @@
 # Copyright The OpenTelemetry Authors
 # SPDX-License-Identifier: Apache-2.0
 
+# pylint: disable=too-many-lines
+
 import logging
 from unittest import IsolatedAsyncioTestCase, mock
 
